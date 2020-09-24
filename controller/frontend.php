95e4b7e0fc35a67129d78817ebdf52bf3323e199
Julie Xaxa <julie@helixsi.com>
2020-09-24 12:37:35 +0200

    initialisation projet

---
+++ controller/frontend.php
@@ -0,0 +1,42 @@
+<?php
+
+
+
+// **** Copie TP_Blog *******
+
+
+require('models/frontend.php');
+
+function listPosts()
+{
+    $posts = getPosts();
+    $isLoggedIn = isLoggedIn();
+
+    require('views/frontend/listPostsView.php');
+}
+
+function post()
+{
+    if (isset($_GET['id']) && $_GET['id'] > 0) 
+    {
+        $post = getPost($_GET['id']);
+        $comments = getComments($_GET['id']);
+        require('views/frontend/postView.php');
+    }
+    else 
+    {
+        echo 'Erreur : aucun identifiant de billet envoyé';
+    }
+}
+
+function addComment($post_id, $author, $comment)
+{
+    $affectedLines = postComment($post_id, $author, $comment);
+
+    if ($affectedLines === false) {
+        die('Impossible d\'ajouter le commentaire !');
+    }
+    else {
+        header('Location: index.php?action=post&id=' . $post_id);
+    }
+}

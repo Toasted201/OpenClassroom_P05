11d7dc1d018f04d51ee665b6e492fb4f52a42080
Julie Xaxa <julie@helixsi.com>
2020-10-05 10:47:20 +0200

    Refactor Else in Request 'Else clauses are basically not necessary and you can simplify the code by not using them.'

--- app/Request.php
+++ app/Request.php
@@ -25,35 +25,31 @@ class Request
     {
         if (!isset($_SERVER['DB_HOST'])) {
             throw new \Exception('DB_Host non défini');
-        } else {
-            return $_SERVER['DB_HOST'];   
         }
+            return $_SERVER['DB_HOST'];   
     }
 
     public static function dbusername()
     {
         if (!isset($_SERVER['DB_USERNAME'])) {
             throw new \Exception('DB_USERNAME non défini');
-        } else {
-            return $_SERVER['DB_USERNAME'];   
         }
+            return $_SERVER['DB_USERNAME'];   
     }
 
     public static function dbname()
     {
-        if (!isset($_SERVER['DB_USERNAME'])) {
+        if (!isset($_SERVER['DB_NAME'])) {
             throw new \Exception('DB_NAME non défini');
-        } else {
-            return $_SERVER['DB_NAME'];   
         }
+            return $_SERVER['DB_NAME'];   
     }
 
     public static function dbpassword()
     {
         if (!isset($_SERVER['DB_PASSWORD'])) {
             throw new \Exception('DB_PASSWORD non défini');
-        } else {
-            return $_SERVER['DB_PASSWORD'];   
         }
+            return $_SERVER['DB_PASSWORD'];
     }     
 }

07274acd2b82d9a0ca3dbf0fde1c7adf0f766e0d
Julie Xaxa <julie@helixsi.com>
2020-10-05 11:08:07 +0200

    Install Sniffer & Mess

--- public/index.php
+++ public/index.php
@@ -1,17 +1,14 @@
 <?php
+
 use Controller\FrontController;
 use App\{Request, Session, Router};
 
 // charger autoloader
 require '../vendor/autoload.php';
-
 Session::start();
-
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
 $dotenv->load();
-
 $action = Request::get('action');
-
 /*TO DO - FINIR LE ROUTEUR
 $router = new Router($action);
 $router->get('', [FrontController::class, 'home']);
@@ -23,65 +20,62 @@ exit;
 if ($action === '') {
     $controller = new FrontController();
     $controller->home();
-}
-elseif ($action === 'posts') {
+} elseif ($action === 'posts') {
     $controller = new FrontController();
     $controller->posts();
-}
-elseif ($action === 'connexion') {
+} elseif ($action === 'connexion') {
     $controller = new FrontController();
     $controller->connexion();
-}
-elseif ($action === 'curriculum') {
+} elseif ($action === 'curriculum') {
     $controller = new FrontController();
     $controller->curriculum();
-}
-elseif ($action === 'admin') {
+} elseif ($action === 'admin') {
     $controller = new FrontController();
     $controller->admin();
+} else {
+    echo 'PageInconnue';
 }
-else echo 'PageInconnue';
 
 /*
 // ****************** COPIE TP_BLOG
 
-if (isset($_GET['action'])) 
+if (isset($_GET['action']))
 {
     if($action === '') {
         $controller = new FrontController();
         $controller->index();
     }
-    if ($_GET['action'] == 'listPosts') 
+    if ($_GET['action'] == 'listPosts')
     {
         listPosts();
     }
-    elseif ($_GET['action'] == 'post') 
+    elseif ($_GET['action'] == 'post')
     {
-        if (isset($_GET['id']) && $_GET['id'] > 0) 
+        if (isset($_GET['id']) && $_GET['id'] > 0)
         {
             post();
         }
-        else 
+        else
         {
             echo 'Erreur : aucun identifiant de billet envoyé';
         }
     }
-    elseif ($_GET['action'] == 'addComment') 
+    elseif ($_GET['action'] == 'addComment')
     {
-        if (isset($_GET['id']) && $_GET['id'] > 0) 
+        if (isset($_GET['id']) && $_GET['id'] > 0)
         {
-            if (!empty($_POST['author']) && !empty($_POST['comment'])) 
+            if (!empty($_POST['author']) && !empty($_POST['comment']))
             {
                 addComment($_GET['id'], $_POST['author'], $_POST['comment']);
             }
-            else 
+            else
             {
                 echo 'Erreur : tous les champs ne sont pas remplis !';
             }
         }
     }
-}    
-else 
+}
+else
 {
     listPosts();
 }

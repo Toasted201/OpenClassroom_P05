1e3767a93ff31c5e6de6edbbd5e859e5d92cad0c
Julie Xaxa <julie@helixsi.com>
2020-10-08 12:09:50 +0200

    $_ENV[]

--- app/Request.php
+++ app/Request.php
@@ -21,36 +21,4 @@ class Request
     {
         return $_SERVER['REQUEST_METHOD'];
     }
-
-    public static function dbhost()
-    {
-        if (!isset($_SERVER['DB_HOST'])) {
-            throw new \Exception('DB_Host non défini');
-        }
-            return $_SERVER['DB_HOST'];
-    }
-
-    public static function dbusername()
-    {
-        if (!isset($_SERVER['DB_USERNAME'])) {
-            throw new \Exception('DB_USERNAME non défini');
-        }
-            return $_SERVER['DB_USERNAME'];
-    }
-
-    public static function dbname()
-    {
-        if (!isset($_SERVER['DB_NAME'])) {
-            throw new \Exception('DB_NAME non défini');
-        }
-            return $_SERVER['DB_NAME'];
-    }
-
-    public static function dbpassword()
-    {
-        if (!isset($_SERVER['DB_PASSWORD'])) {
-            throw new \Exception('DB_PASSWORD non défini');
-        }
-            return $_SERVER['DB_PASSWORD'];
-    }
 }
